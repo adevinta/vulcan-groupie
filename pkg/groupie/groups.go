3906dead622ac7c90d86f6259d7d89c2dc67fe2f
Gerard Guillemas <gerard.guillemas@schibsted.com>
2020-04-15 08:57:13 +0200

    Remove unnecessary issue

--- pkg/groupie/groups.go
+++ pkg/groupie/groups.go
@@ -144,7 +144,6 @@ var (
 		"SSL Medium Strength Cipher Suites Supported (SWEET32)":                                 "ssl",
 		"TLS Version 1.0 Protocol Detection":                                                    "ssl",
 		"TLS Version 1.1 Protocol Detection":                                                    "ssl",
-		"Unknown TLS Issue":                                                                     "ssl",
 
 		// Email Spoofing group
 		//DKIM
